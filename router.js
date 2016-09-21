db38bf5b50b05c5dc2111100761053320e9ef27f
Karl Pokus <karl.pokus@gmail.com>
2016-09-21 15:16:41 +0000

    debugging setup

--- router.js
+++ router.js
@@ -3,28 +3,23 @@ var max = require('./lib/bot.js');
 
 var router = {
   dispatch: function(req, res) {
-
-    // head or post
     if (req.method === 'HEAD' || req.method === 'POST') {
 
+      // debug
+      console.log('router.dispatch fired with method:', req.method);
+      
       this[req.method](req, res);
 
     } else {
-
       res.statusCode = 403;
       res.end();
-
     }
-
   },
   HEAD: function(req, res) {
-
     res.statusCode = 200;
     res.end();
-
   },
   POST: function(req, res) {
-
     var data = "",
         that = this;
 
@@ -35,36 +30,33 @@ var router = {
       data += chunk;
 
     }).on('end', function() {
-
       res.statusCode = 200;
       res.end();
 
       if (data !== "") {
-
-        req.body = data;
-        that.passOnToBot(req);
-
+        data = JSON.parse(data);
+        
+        // debug
+        console.log('data:', data);
+        
+        that.passOnToBot(data, req);
       }
-
     });
   },
-  passOnToBot: function(req) {
-
-    var data = JSON.parse(req.body);
-
+  passOnToBot: function(data, req) {
     if (max.originIsTrello(req)) {
 
+      // debug
+      console.log('.originIsTrello is ok');
+      
       if (max.actionTypesAreOneOf(['commentCard'], data)) {
         max.kommentarTillBot(data);
         max.ingetAnvändarnamn(data);
       }
-
       if (max.actionTypesAreOneOf(['createCard', 'updateCard'], data)) {
         max.långTitel(data);
       }
-
     }
-
   }
 };
 
--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-// Modules
 var http = require('http'),
-    router = require('./router.js');
-
-// server
-var server = http.createServer(),
+    router = require('./router.js'),
+    server = http.createServer(),
     port = process.env.PORT || 8080;
 
-// events
 server.on('request', router.dispatch.bind(router));
-
-// log
 console.log('Max is running..');
-
-// init
 server.listen(port);

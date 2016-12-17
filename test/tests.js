057b4a125c90c59698fc5c439ffe162e941d2258
Karl Pokus <karl.pokus@gmail.com>
2016-12-18 00:41:47 +0100

    improved testing

--- test/tests.js
+++ test/tests.js
@@ -1,71 +1,74 @@
-// set dummy env vars
 process.env.KEY = 'dummy key';
 process.env.TOKEN = 'dummy token';
 process.env.USERID = '123456';
 process.env.USERNAME = 'maxblipblop';
 
-
-// remove event handler for 'reply' for testing
-
 var test = require('tape'),
-    max = require('../lib/bot.js');
+    max = require('../lib/bot.js'),
+    replies = [],
+    logs = [];
 
-// overwrite postcomment for testing purposes
-max.postComment = function(cardId, answer) {
-  return answer;
-}
+// swap handlers to intercept for testing
+max
+  .removeAllListeners('reply')
+  .on('reply', function(cardId, answer, res){
+    replies.push(answer);
+  })
+  .removeAllListeners('log')
+  .on('log', function(msg){
+    logs.push(msg);
+  });
 
-test('kommentarTillBot', function(t){
-  var data = {
-    action: {
-      data: {
-        text: '@maxblipblop vem är sekreterare på vug',
-        card: {id: 'cardId'}
-      },
-      memberCreator: {id: 'some id', username: 'bob'}
-    }
-  },
-  answer1 = max.kommentarTillBot(data),
-  person = max._sekreterare(new Date());
-  
-  data.action.data.text = '@maxblipblop läget?';
-  var answer2 = max.kommentarTillBot(data);
-  
-  t.equal(answer1, '@bob ' + person + ' är sekreterare denna veckan.', '._sekreterare');
-  t.equal(answer2, '@bob ' + 'Blip blop', 'blip blop');
+var data = {
+      action: {
+        data: {
+          text: 'comment goes here',
+          card: {
+            id: 'id'
+          }
+        },
+        memberCreator: {
+          id: process.env.USERID,
+          username: 'username'
+        }
+      }
+    },
+    res = {end: function(){}};
+
+test('replyToComment', function(t){
+
+  var expectedReply = 'comment author is bot';
+  max.emit('commentCard', data, res);
+  t.equal(logs[0], expectedReply, 'comment author is bot');
+
+  data.action.memberCreator.id = 'some other id';
+  max.emit('commentCard', data, res);
+  t.ok(/Använd @username så får de en notis/.test(replies[0]), 'no@inComment');
+
+  data.action.data.text = '@' + process.env.USERNAME + ' sekreterare idag?';
+  max.emit('commentCard', data, res);
+  t.ok(/C-F|Anna|Kicki/.test(replies[1]), 'secretary duty');
+
+  data.action.data.text = '@' + process.env.USERNAME + ' hi!';
+  max.emit('commentCard', data, res);
+  t.ok(/Blip blop/.test(replies[2]), '@bot but no other match');
+
   t.end();
 });
 
+test('longTitle', function(t){
 
-/*
-test('http', function(t){
+  data.action.data.card.name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  max.emit('createCard', data, res);
+  max.emit('updateCard', data, res);
+  t.ok(/Långa titlar är svåra att läsa/.test(replies[3]), '>100 createCard');
+  t.ok(/Långa titlar är svåra att läsa/.test(replies[4]), '>100 updateCard');
 
-  t.plan(4);
+  data.action.data.card.name = "lorem";
+  max.emit('createCard', data, res);
+  max.emit('updateCard', data, res);
+  t.ok(/title is less than/.test(logs[1]), '<100 createCard');
+  t.ok(/title is less than/.test(logs[2]), '<100 updateCard');
 
-  r({url: url, method: 'HEAD'}, function (err, res) {
-    t.equal(res.statusCode, 200, 'HEAD');
-  });
-
-  r({url: url, method: 'POST'}, function (err, res) {
-    t.equal(res.statusCode, 200, 'POST with empty string as data');
-  });
-
-  r({
-    url: url,
-    method: 'POST',
-    json: true,
-    body: JSON.stringify({cat: 'bixa'}),
-    headers: {
-      "x-trello-webhook": 'temp'
-    }
-  }, function (err, res) {
-    t.equal(res.statusCode, 200, 'POST with data');
-  });
-
-  r({url: url, method: 'GET'}, function (err, res) {
-    t.equal(res.statusCode, 403, 'GET');
-  });
-
+  t.end();
 });
-
-*/

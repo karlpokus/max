fc36c437a8dc6fadf31af8bab3d2deb7c7a152b9
Karl Pokus <karl.pokus@gmail.com>
2016-12-23 16:30:37 +0000

    add reply to units in section at psyk

--- test/tests.js
+++ test/tests.js
@@ -46,39 +46,75 @@ test('replyToComment', function(t){
 
   data.action.memberCreator.id = process.env.USERID;
   max.emit('commentCard', data, res);
-  t.ok(/Comment author is bot/.test(log), 'comment author is bot');
-  t.ok(!reply);
+  t.ok(/Comment author is bot/.test(log), 'log: comment author is bot');
+  t.ok(!reply, 'reply');
   resetState();
 
   data.action.memberCreator.id = 'some other id';
   data.action.data.text = 'just a comment';
   max.emit('commentCard', data, res);
-  t.ok(/Använd @username så får de en notis/.test(reply), 'no @ in comment');
-  t.ok(!log);
+  t.ok(/Använd @username/.test(reply), 'reply: no @ in comment');
+  t.ok(!log, 'log');
   resetState();
 
   data.action.data.text = '@' + process.env.USERNAME + ' sekreterare idag?';
   max.emit('commentCard', data, res);
-  t.ok(/C-F|Anna|Kicki/.test(reply), '@bot secretary duty');
-  t.ok(!log);
+  t.ok(/C-F|Anna|Kicki/.test(reply), 'reply: secretary duty');
+  t.ok(!log, 'log');
+  resetState();
+
+  data.action.data.text = '@' + process.env.USERNAME + ' enhet';
+  max.emit('commentCard', data, res);
+  t.ok(/Letar du efter enheter på psyk/.test(reply), 'reply: units but no section');
+  t.ok(!log, 'log');
+  resetState();
+
+  data.action.data.text = '@' + process.env.USERNAME + ' enhet akut';
+  max.emit('commentCard', data, res);
+  t.ok(/Akut- och konsultpsykiatri/.test(reply), 'reply: units and section akut');
+  t.ok(!log, 'log');
+  resetState();
+
+  data.action.data.text = '@' + process.env.USERNAME + ' enhet Neuro';
+  max.emit('commentCard', data, res);
+  t.ok(/Beroende- och neuropsykiatri/.test(reply), 'reply: units and section neuro');
+  t.ok(!log, 'log');
+  resetState();
+  
+  data.action.data.text = '@' + process.env.USERNAME + ' enhet psYkOS';
+  max.emit('commentCard', data, res);
+  t.ok(/Psykosvård- och rättspsyk vård/.test(reply), 'reply: units and section psykos');
+  t.ok(!log, 'log');
+  resetState();
+  
+  data.action.data.text = '@' + process.env.USERNAME + ' enhet affEKtiva';
+  max.emit('commentCard', data, res);
+  t.ok(/Affektiva sjukdomar/.test(reply), 'reply: units and section affektiva');
+  t.ok(!log, 'log');
+  resetState();
+  
+  data.action.data.text = '@' + process.env.USERNAME + ' enhet BuP';
+  max.emit('commentCard', data, res);
+  t.ok(/Barn- och ungdomspsykiatri/.test(reply), 'reply: units and section bup');
+  t.ok(!log, 'log');
   resetState();
 
   data.action.data.text = '@' + process.env.USERNAME + ' hi!';
   max.emit('commentCard', data, res);
-  t.ok(/Blip blop/.test(reply), '@bot but no other match');
-  t.ok(!log);
+  t.ok(/Blip blop/.test(reply), 'reply: blip blop');
+  t.ok(!log, 'log');
   resetState();
 
   data.action.data.text = '@foo zup?';
   max.emit('commentCard', data, res);
-  t.ok(/Comment for another user/.test(log), '@user zup');
-  t.ok(!reply);
+  t.ok(/Comment for another user/.test(log), 'log: comment for another user');
+  t.ok(!reply, 'reply');
   resetState();
 
   data.model.id = process.env.USERID;
   max.emit('commentCard', data, res);
-  t.ok(/Model is bot/.test(log), 'Model is bot');
-  t.ok(!reply);
+  t.ok(/Model is bot/.test(log), 'log: Model is bot');
+  t.ok(!reply, 'reply');
   resetState();
 
   t.end();
